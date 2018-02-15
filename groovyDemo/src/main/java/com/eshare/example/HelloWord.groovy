43f344c16be4aef43b74771bf35c33f802785c43
Evan43789596 <10856214@163.com>
2018-02-15 18:21:34 +0800

    feat:创建Groovy Hello Word 示例

---
+++ groovyDemo/src/main/java/com/eshare/example/HelloWord.groovy
@@ -0,0 +1,11 @@
+package com.eshare.example
+/**
+ * Created by liangyh on 2018/2/15.
+ * Email:10856214@163.com
+ */
+class HelloWord {
+
+    static void main(String[] args) {
+        println("Hello Groovy")
+    }
+}
---
+++ groovyDemo/src/test/java/com/eshare/Test.groovy
@@ -0,0 +1,8 @@
+package com.eshare
+
+/**
+ * Created by liangyh on 2018/2/15.
+ * Email:10856214@163.com
+ */
+class Test {
+}
