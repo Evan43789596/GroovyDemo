0933b212dbe7b19cb626e154fc90a14efc013beb
Evan43789596 <10856214@163.com>
2018-02-16 09:36:13 +0800

    feat:新增特性 demo

---
+++ groovyDemo/src/main/java/com/eshare/example/TraitDemo.groovy
@@ -0,0 +1,36 @@
+package com.eshare.example
+
+/**
+ * Created by liangyh on 2018/2/16.
+ * Email:10856214@163.com
+ */
+class TraitDemo {
+
+     trait Animals {
+
+         void eat(){
+             println("吃东西");
+         }
+     }
+
+    interface Cats extends Animals{
+
+        void run();
+    }
+
+
+    static class Tiger implements Cats,Animals{
+
+        @Override
+        void run() {
+            println("快速奔跑");
+        }
+    }
+
+    static void main(String[] args){
+        Tiger tiger = new Tiger();
+        tiger.eat();
+        tiger.run();
+    }
+
+}
